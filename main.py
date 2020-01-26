b995946e102f0c2402437246d7278da3207c51f0
홍세정 <sudo0272@gmail.com>
2020-01-27 07:21:12 +0900

    show characters at once typing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ target = open('source.txt', 'r')
 os.system('clear')
 
 for i in target.readlines():
-    for j in i:
+    for j in [i[k:k + 4] for k in range(0, len(i), 4)]:
         getch()
 
         sys.stdout.write(j)
